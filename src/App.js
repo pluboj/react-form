145e66e3531eafebd9d4566e147c5684b682ce0c
plubojacky <plubojacky@email.com>
2017-06-22 09:30:30 -0700

    Update layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Form, FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap';
+import { Form, FormGroup, ControlLabel, FormControl, Button, Col } from 'react-bootstrap';
 
 import './App.css';
 
@@ -44,48 +44,56 @@ class App extends Component {
     const wellStyles = {maxWidth: 600, margin: '20px auto 10px'};
 
     return (
-      <div className="well" style={wellStyles}>
-        <Form inline onSubmit={this.onFormSubmit}>
-          <h3>Sign Up</h3>
+      <div style={wellStyles}>
+        <div className="well" style={wellStyles}>
+          <Form horizontal onSubmit={this.onFormSubmit}>
+            <h3 className="header">Sign Up</h3>
 
-          <FormGroup controlId="formInlineName">
-            <ControlLabel>Name</ControlLabel>
-            {' '}
-            <FormControl
-              type="text"
-              name="name"
-              placeholder="Name"
-              value={this.state.fields.name}
-              onChange={this.onInputChange}
-              inputRef={(input) => { this.nameInput = input; }}
-            />
-          </FormGroup>
-          {' '}
-          <FormGroup controlId="formInlineEmail">
-            <ControlLabel>Email</ControlLabel>
-            {' '}
-            <FormControl
-              type="email"
-              name="email"
-              placeholder="user@email.com"
-              value={this.state.fields.email}
-              onChange={this.onInputChange}
-            />
-          </FormGroup>
-
-          {' '}
-          <Button type="submit">
-          Submit
-          </Button>
-        </Form>
-
-        <div className="output">
-          <h4>Members: </h4>
-          <ul>
-            { this.state.members.map(({name, email}, i) => 
-              <li key={i}>{name} ({email})</li>)}
-          </ul>
+            <FormGroup controlId="formInlineName">
+              <Col componentClass={ControlLabel} sm={2}>
+                Name
+              </Col>
+              <Col sm={10}>
+                <FormControl
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  value={this.state.fields.name}
+                  onChange={this.onInputChange}
+                  inputRef={(input) => { this.nameInput = input; }}
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup controlId="formInlineEmail">
+              <Col componentClass={ControlLabel} sm={2}>
+                Email
+              </Col>
+              <Col sm={10}>
+                <FormControl
+                  type="email"
+                  name="email"
+                  placeholder="user@email.com"
+                  value={this.state.fields.email}
+                  onChange={this.onInputChange}
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup>
+              <Col smOffset={2} sm={10}>
+                <Button type="submit">
+                Submit
+                </Button>
+              </Col>
+             </FormGroup>
+          </Form>
         </div>
+        <div className="output">
+            <h4>Members: </h4>
+            <ul>
+              { this.state.members.map(({name, email}, i) => 
+                <li key={i}>{name} ({email})</li>)}
+            </ul>
+          </div>
       </div>
     );
   }
